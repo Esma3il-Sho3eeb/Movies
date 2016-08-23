cb7c7eacd97b6e94aaa37b9f3772bf645060f855
Ismail Shoaib <ismail.nabeel4656@ci.menofia.edu.eg>
2016-08-23 19:40:43 +0200

    add volley
    add Volley library to easy develop
    and hander internet request

---
+++ app/src/main/java/com/ecsm/android/VolleyQueue.java
@@ -0,0 +1,39 @@
+package com.ecsm.android;
+
+import android.content.Context;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+public class VolleyQueue {
+    private static VolleyQueue mInstance;
+    private static Context mCtx;
+    private RequestQueue mRequestQueue;
+
+    private VolleyQueue(Context context) {
+        mCtx = context;
+        mRequestQueue = getRequestQueue();
+
+
+    }
+
+    public static synchronized VolleyQueue getInstance(Context context) {
+        if (mInstance == null) {
+            mInstance = new VolleyQueue(context);
+        }
+        return mInstance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (mRequestQueue == null) {
+            mRequestQueue = Volley.newRequestQueue(mCtx.getApplicationContext());
+        }
+        return mRequestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        getRequestQueue().add(req);
+    }
+
+}
