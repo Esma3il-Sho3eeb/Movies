fba4f531677e4ffee78f77639b97b7b3230c0dbd
Ismail Shoaib <ismail.nabeel4656@ci.menofia.edu.eg>
2016-08-25 02:06:01 +0200

    create  setting activity

---
+++ app/src/main/java/com/ecsm/android/movie/ui/FragmentSettings.java
@@ -0,0 +1,22 @@
+package com.ecsm.android.movie.ui;
+
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+import com.ecsm.android.movie.R;
+
+public class FragmentSettings extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preference);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
+        ActivityBrows.refreshStatus=true;
+
+    }
+}
