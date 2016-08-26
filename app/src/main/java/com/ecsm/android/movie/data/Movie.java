4b6e62572663f185b5d73597d80c2d10480887a8
Ismail Shoaib <ismail.nabeel4656@ci.menofia.edu.eg>
2016-08-26 13:06:12 +0200

    Serializable

--- app/src/main/java/com/ecsm/android/movie/data/Movie.java
+++ app/src/main/java/com/ecsm/android/movie/data/Movie.java
@@ -6,11 +6,13 @@ import com.activeandroid.annotation.Table;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
 @Table(name = "movie")
-public class Movie extends Model {
+public class Movie extends Model implements Serializable{
+    public static final String KEY_EXTRA="movie_by";
 
     @Column
     @SerializedName("adult")
